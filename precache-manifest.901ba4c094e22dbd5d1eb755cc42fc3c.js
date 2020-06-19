7ded4cf4bed68eacdb271142bf379c60619a2915
ayheikal <heikal1998@gmail.com>
2020-06-19 17:41:00 +0200

    Updates

---
+++ precache-manifest.901ba4c094e22dbd5d1eb755cc42fc3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592d8af8a08ceb4330c1bdc683047756",
+    "url": "/github-searcher/index.html"
+  },
+  {
+    "revision": "f7eae92377668b9165c9",
+    "url": "/github-searcher/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "76f103bded99af43ff1b",
+    "url": "/github-searcher/static/js/2.c051fbfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-searcher/static/js/2.c051fbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7eae92377668b9165c9",
+    "url": "/github-searcher/static/js/main.f21edddf.chunk.js"
+  },
+  {
+    "revision": "950edfcffee89826d090",
+    "url": "/github-searcher/static/js/runtime-main.7f95676c.js"
+  }
+]);
